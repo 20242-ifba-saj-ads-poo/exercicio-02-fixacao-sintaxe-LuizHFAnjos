88d5f5429f7168218e000df0f4d2f02f0673a3ae
Henrique Anjos <168201702+LuizHFAnjos@users.noreply.github.com>
2024-11-14 14:00:24 +0000

    Exercicio 2

--- Exercicio1.java
+++
@@ -1,8 +0,0 @@
-public class Exercicio1 {
-    public static void main(String[] args) {
-        int n;
-        for(n = 150; n <= 300; n++){
-            System.out.println(n);
-        }
-    }
-}
